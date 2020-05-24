08e818e2ad3b94bd8084a7a295e585c1dc84013f
Neil Castelino <neil.fl93@gmail.com>
2020-05-24 17:17:03 -0400

    Use descriptive http codes (refs #17)

--- routes/ClientInfo.go
+++ routes/ClientInfo.go
@@ -1,12 +1,14 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // IPAddress replies with the client's ip address
 func IPAddress(c *gin.Context) {
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"IP": c.ClientIP(),
 	})
 }
--- routes/Maxmind.go
+++ routes/Maxmind.go
@@ -3,20 +3,23 @@ package routes
 import (
 	"rest-geoip/maxmind"
 
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // GeoIPInfo replies with the client's geo ip info
 func GeoIPInfo(c *gin.Context) {
 	record, err := maxmind.Info(c.ClientIP())
+	// TODO: use service unavaialble if db is not present
 	if err != nil {
-		c.JSON(500, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 
-	c.JSON(200, record)
+	c.JSON(http.StatusOK, record)
 }
 
 // DisplayGeoIPInfo displays geoip info via HTML
@@ -25,23 +28,23 @@ func DisplayGeoIPInfo(c *gin.Context) {
 
 	// TODO: Show a custom 404 error on frontend
 	if err != nil {
-		c.JSON(500, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 
-	c.HTML(200, "/templates/ipAddressInfo.tmpl", record)
+	c.HTML(http.StatusOK, "/templates/ipAddressInfo.tmpl", record)
 }
 
 // UpdateMaxmindDB updates the db
 func UpdateMaxmindDB(c *gin.Context) {
 	if err := maxmind.DownloadAndUpdate(); err != nil {
-		c.JSON(500, gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 
-	c.JSON(200, gin.H{})
+	c.JSON(http.StatusOK, gin.H{})
 }
