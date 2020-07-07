dc6a633bf4e16a3a9abb941d9976901ac23d0abd
Neil Castelino <neil.fl93@gmail.com>
2020-07-06 21:45:46 -0400

    Address shellcheck issues with init script

--- init_deps.sh
+++ init_deps.sh
@@ -8,25 +8,25 @@ BOOTSTRAP_OUT="bootstrap.zip"
 LEAFLET_VERSION="1.6.0"
 LEAFLET_OUT="leaflet.zip"
 
-function pre_init {
+pre_init() {
     rm -rf static/
     mkdir static
 }
 
-function init_bootstrap {
+init_bootstrap() {
     "${WGET}" -q "https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip" -O "${BOOTSTRAP_OUT}"
     unzip -o -d static "${BOOTSTRAP_OUT}" \
         bootstrap-4.5.0-dist/css/bootstrap.min.css \
         bootstrap-4.5.0-dist/css/bootstrap.min.css.map
 }
 
-function init_leaflet {
+init_leaflet() {
     "${WGET}" -q "http://cdn.leafletjs.com/leaflet/v${LEAFLET_VERSION}/leaflet.zip" -O "${LEAFLET_OUT}"
     mkdir -p "static/leaflet-v${LEAFLET_VERSION}"
     unzip -o -d "static/leaflet-v${LEAFLET_VERSION}" "${LEAFLET_OUT}"
 }
 
-function cleanup {
+cleanup() {
     rm -f \
         "${BOOTSTRAP_OUT}" \
         "${LEAFLET_OUT}"
