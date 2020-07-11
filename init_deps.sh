ad44bc4262b09411a31edd7d560966c6f3f71515
Neil Castelino <neil.fl93@gmail.com>
2020-07-10 20:19:26 -0400

    Separate 3rd party static and add spacing between nav and container

--- init_deps.sh
+++ init_deps.sh
@@ -7,23 +7,23 @@ BOOTSTRAP_VERSION="4.5.0"
 BOOTSTRAP_OUT="bootstrap.zip"
 LEAFLET_VERSION="1.6.0"
 LEAFLET_OUT="leaflet.zip"
+STATIC_DIR="static/3rdparty"
 
 pre_init() {
-    rm -rf static/
-    mkdir static
+    rm -rf "${STATIC_DIR}/*"
 }
 
 init_bootstrap() {
     "${WGET}" -q "https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip" -O "${BOOTSTRAP_OUT}"
-    unzip -o -d static "${BOOTSTRAP_OUT}" \
+    unzip -o -d "${STATIC_DIR}" "${BOOTSTRAP_OUT}" \
         bootstrap-4.5.0-dist/css/bootstrap.min.css \
         bootstrap-4.5.0-dist/css/bootstrap.min.css.map
 }
 
 init_leaflet() {
     "${WGET}" -q "http://cdn.leafletjs.com/leaflet/v${LEAFLET_VERSION}/leaflet.zip" -O "${LEAFLET_OUT}"
-    mkdir -p "static/leaflet-v${LEAFLET_VERSION}"
-    unzip -o -d "static/leaflet-v${LEAFLET_VERSION}" "${LEAFLET_OUT}"
+    mkdir -p "${STATIC_DIR}/leaflet-v${LEAFLET_VERSION}"
+    unzip -o -d "${STATIC_DIR}/leaflet-v${LEAFLET_VERSION}" "${LEAFLET_OUT}"
 }
 
 cleanup() {
