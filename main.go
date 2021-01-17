7e92efcc2a9cf6f1a14c38fbe0df5da85c8c3533
Neil Castelino <neil.fl93@gmail.com>
2021-01-16 19:01:35 -0500

    Set default env var values

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 )
 
 func main() {
+	viper.SetDefault("MAXMIND_DB_LOCATION", "/opt/")
+	viper.SetDefault("MAXMIND_DB", "GeoLite2-City.mmdb")
+	viper.SetDefault("LOGGING", "false")
+	viper.SetDefault("WEB", true)
+	viper.SetDefault("LISTEN_ADDRESS", "0.0.0.0")
+	viper.SetDefault("LISTEN_PORT", "8080")
 	viper.AutomaticEnv()
+
 	server := routes.SetupRouter()
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
