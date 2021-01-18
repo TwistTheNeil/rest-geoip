7afe6b567bb0d2f0151d3046cb08247657d16c2a
Neil Castelino <neil.fl93@gmail.com>
2021-01-17 21:38:48 -0500

    Fix typo

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	viper.SetDefault("MAXMIND_DB_LOCATION", "/opt/")
 	viper.SetDefault("MAXMIND_DB", "GeoLite2-City.mmdb")
-	viper.SetDefault("LOGGING", "false")
+	viper.SetDefault("LOGGING", false)
 	viper.SetDefault("WEB", true)
 	viper.SetDefault("LISTEN_ADDRESS", "0.0.0.0")
 	viper.SetDefault("LISTEN_PORT", "8080")
