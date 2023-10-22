bc0f036f1f1f8251e39aa678991c8292f4a05908
Neil Castelino <neil.fl93@gmail.com>
2023-10-22 16:39:59 -0400

    feat: add navbar component

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="https://github.com/TwistTheNeil/rest-geoip">About</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/web">My IP Address</a>
+        </li>
+      </ul>
+      <form class="d-flex" action="/web" method="POST">
+        <input class="form-control me-2" type="search" placeholder="IP Address" aria-label="ip-address" name="search-input">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</template>
