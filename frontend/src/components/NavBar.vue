69ec0ee1d83b62eae165d77620f6d05bea822a7a
Neil Castelino <neil.fl93@gmail.com>
2023-10-22 22:13:41 -0400

    chore: simplify navbar and search

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,21 +1,9 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
-  <div class="container-fluid">
+<nav class="navbar navbar-light bg-light mb-5">
+  <div class="container">
     <a class="navbar-brand" href="https://github.com/TwistTheNeil/rest-geoip">About</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/web">My IP Address</a>
-        </li>
-      </ul>
-      <form class="d-flex" action="/web" method="POST">
-        <input class="form-control me-2" type="search" placeholder="IP Address" aria-label="ip-address" name="search-input">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+    <!-- replace href to something vue router capable -->
+    <a class="nav-link" aria-current="page" href="/">My IP Address</a>
   </div>
 </nav>
 </template>
--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="form-floating mt-4 mb-4">
+  <div class="input-group mb-3">
+    <span class="input-group-text">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+      </svg>
+    </span>
     <input
       type="text"
-      class="form-control border-0 shadow-none"
+      class="form-control"
       id="searchbar"
       placeholder="Query details about IP Address..."
+      aria-label="Query details about an IP Address"
       v-model="queryString"
       @keyup.enter="$emit('queryRequest', queryString)"
     />
-    <label for="searchBar">Query details about IP Address...</label>
   </div>
 </template>
 
