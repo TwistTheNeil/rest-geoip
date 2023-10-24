a3958242f558d8b51bd751d0a4399610096302de
Neil Castelino <neil.fl93@gmail.com>
2023-10-24 01:28:34 -0400

    chore: move search icon out to component

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="input-group mb-3">
     <span class="input-group-text">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-      </svg>
+      <SearchIcon />
     </span>
     <input
       type="text"
@@ -20,6 +18,8 @@
 <script setup lang='ts'>
   import { ref } from 'vue';
   import type { Ref } from 'vue';
+
+  import SearchIcon from '@/components/icons/SearchIcon.vue';
 
   const props = defineProps({
     currentIPAddress: {
