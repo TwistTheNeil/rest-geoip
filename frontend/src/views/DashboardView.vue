18968f3ab6dae7dbc652c21145109a9fb115a119
Neil Castelino <neil.fl93@gmail.com>
2023-10-27 23:13:30 -0400

    chore: we do not need a second isready

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,38 +1,36 @@
 <template>
   <div>
-    <div v-if="isReady">
-      <div class="card">
-        <div class="card-header">
-          Details for IP Address: {{ data!.IP }}
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-lg-6">
-              <table class="table table-borderless table-hover table-sm">
-              <tbody>
-                <tr> <td> Country </td> <td>{{ data!.Country.ISOCode }}</td>  </tr>
-                <tr> <td> EU </td> <td>{{ data!.Country.IsInEuropeanUnion }}</td> </tr>
-                <tr>
-                  <td> City </td>
-                  <td>
-                    <span v-for="name in cityNames">
-                      {{ name }}
-                      <br />
-                    </span>
-                  </td>
-                </tr>
-                <tr> <td> Latitude </td> <td>{{ data!.Location.Latitude }}</td> </tr>
-                <tr> <td> Longitude </td> <td>{{ data!.Location.Longitude }}</td> </tr>
-                <tr> <td> Time Zone </td> <td>{{ data!.Location.TimeZone }}</td> </tr>
-                <tr> <td> Approx. Zip Code </td> <td>{{ data!.Postal.Code }}</td> </tr>
-                <tr> <td> Anonymous Proxy </td> <td>{{ data!.Traits.IsAnonymousProxy }}</td> </tr>
-                <tr> <td> Satellite Provider </td> <td>{{ data!.Traits.IsSatelliteProvider }}</td> </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="col-lg-6">
-              <ApproximateMap :longitude="data!.Location.Longitude" :latitude="data!.Location.Latitude" />
-            </div>
+    <div class="card">
+      <div class="card-header">
+        Details for IP Address: {{ data!.IP }}
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-6">
+            <table class="table table-borderless table-hover table-sm">
+            <tbody>
+              <tr> <td> Country </td> <td>{{ data!.Country.ISOCode }}</td>  </tr>
+              <tr> <td> EU </td> <td>{{ data!.Country.IsInEuropeanUnion }}</td> </tr>
+              <tr>
+                <td> City </td>
+                <td>
+                  <span v-for="name in cityNames">
+                    {{ name }}
+                    <br />
+                  </span>
+                </td>
+              </tr>
+              <tr> <td> Latitude </td> <td>{{ data!.Location.Latitude }}</td> </tr>
+              <tr> <td> Longitude </td> <td>{{ data!.Location.Longitude }}</td> </tr>
+              <tr> <td> Time Zone </td> <td>{{ data!.Location.TimeZone }}</td> </tr>
+              <tr> <td> Approx. Zip Code </td> <td>{{ data!.Postal.Code }}</td> </tr>
+              <tr> <td> Anonymous Proxy </td> <td>{{ data!.Traits.IsAnonymousProxy }}</td> </tr>
+              <tr> <td> Satellite Provider </td> <td>{{ data!.Traits.IsSatelliteProvider }}</td> </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="col-lg-6">
+            <ApproximateMap :longitude="data!.Location.Longitude" :latitude="data!.Location.Latitude" />
           </div>
         </div>
       </div>
@@ -51,10 +49,6 @@
 
   const maxmindDataStore = useMaxmindDataStore();
   const { data }: { data: Ref<MaxmindBackendResponse | null> } = storeToRefs(maxmindDataStore);
-
-  const isReady = computed(() => {
-    return data.value !== null;
-  });
 
   const cityNames = computed(() => {
     if (data.value === null) {
