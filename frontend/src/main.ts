7e536b0a911adf40008304192f5990212941426b
Neil Castelino <TwistTheNeil@users.noreply.github.com>
2026-01-05 21:36:59 -0500

    chore(deps-dev): add eruda for mobile browser debugging

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,6 +9,11 @@ import router from './router';
 const pinia = createPinia();
 const app = createApp(App);
 
+// add eruda for debugging on mobile browsers
+if (import.meta.env.DEV) {
+  import('eruda').then((eruda) => eruda.default.init());
+}
+
 app.use(pinia);
 app.use(router);
 
